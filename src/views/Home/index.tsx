6cf8e6206be1471979e7bb88c648d3e40c71a9bf
jojaehyeok99 <choco5366@naver.com>
2024-05-14 09:43:34 +0900

    App.tsx 수정 및 홈화면 수정

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import "../../layouts/ServiceContainer/index"
-export default function index() {
+export default function Home() {
   return (
-    <>
+    <div id='wrapper'>
       <div>
         <div>당신의 일상에 편리함을 제공합니다.</div>
         <div>image</div>
       </div>
       <div>대여방법</div>
       <div>파손 면책 안내</div>.
-    </>
+    </div>
   );
 }
