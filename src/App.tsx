9db8c628469bcf2ee026f2206d3c6fe5e0099d2a
jojaehyeok99 <choco5366@naver.com>
2024-05-14 12:49:23 +0900

    react 시작 시 홈화면으로 이동 수정 및 홈화면 css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router';
-import index from './layouts/ServiceContainer';
+import { Route, Routes, useNavigate } from 'react-router';
 import { AUTH_ABSOLUTE_PATH, HOME_ABSOLUTE_PATH, RDRG_ABSOLUTE_PATH } from './constants';
 import Authentication from './views/Authentication';
-import RdrgContainer from './layouts/ServiceContainer';
+import RdrgContainer from './layouts/HomeContainer';
 import Home from './views/Home';
+
+// component : root 경로 컴포넌트 //
+function Index() {
+
+  //                    function                    //
+  const navigator =useNavigate();
+
+  //                    effect                    //
+  useEffect(() => {
+    navigator(HOME_ABSOLUTE_PATH);
+  }, []);
+
+  //                    render                    //
+  return <></>;
+}
 
 function App() {
   return(
     <Routes>
+      <Route index element = {<Index />} />
       <Route path={RDRG_ABSOLUTE_PATH} element = {<RdrgContainer/>} >
         <Route path={HOME_ABSOLUTE_PATH} element = {<Home />} />
         <Route path={AUTH_ABSOLUTE_PATH} element = {<Authentication/>} />
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import "../../layouts/ServiceContainer/index"
+import "../../layouts/HomeContainer/index"
+import './style.css'
+
 export default function Home() {
   return (
     <div id='wrapper'>
-      <div>
-        <div>당신의 일상에 편리함을 제공합니다.</div>
-        <div>image</div>
+      <div className='main-top'>
+        <div className='main-top-content'>당신의 일상에 편리함을 제공합니다.</div>
+        <div className='main-top-image'></div>
       </div>
-      <div>대여방법</div>
-      <div>파손 면책 안내</div>.
+      <div className='main-body'>대여방법</div>
+      <div className='main-foot'>파손 면책 안내</div>
     </div>
   );
 }
