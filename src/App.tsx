fd4536189c4f0e59c83d15a74ff17ada4f8e6ad0
OREO206 <baek6654@naver.com>
2024-05-16 17:35:42 +0900

    지상 오후

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { Route, Routes, useNavigate } from 'react-router';
-import { AUTH_ABSOLUTE_PATH, CUSTOMER_SUPPORT_ABSOLUTE_PATH, HOME_ABSOLUTE_PATH, MYPAGE_PROFILE_ABSOLUTE_PATH, RDRG_ABSOLUTE_PATH, SIGN_UP_ABSOLUTE_PATH } from './constants';
+import { AUTH_ABSOLUTE_PATH, CUSTOMER_SUPPORT_DETAIL_PATH, CUSTOMER_SUPPORT_PATH, CUSTOMER_SUPPORT_UPDATE_PATH, CUSTOMER_SUPPORT_WRITE_ABSOLUTE_PATH, HOME_ABSOLUTE_PATH, MYPAGE_PROFILE_ABSOLUTE_PATH, RDRG_ABSOLUTE_PATH, SIGN_UP_ABSOLUTE_PATH } from './constants';
 import Authentication, { SignUp } from './views/Authentication';
 import RdrgContainer from './layouts/HomeContainer';
 import Home from './views/Home';
 import CustomerSupportList from './views/service/CustomerSupport/CustomerSupportList';
 import MypageProfile from './views/service/MyPage/MyPageProfile';
+import SupportWrite from './views/service/CustomerSupport/CustomerSupportWrite';
+import SupportDetail from './views/service/CustomerSupport/CustomerSupportDetail';
+import SupportUpdate from './views/service/CustomerSupport/CustomerSupportUpdate';
 
 // component : root 경로 컴포넌트 //
 function Index() {
@@ -24,20 +27,24 @@ function Index() {
 }
 
 function App() {
-  return(
+    return(
     <Routes>
         <Route index element = {<Index />} />
         <Route path={RDRG_ABSOLUTE_PATH} element = {<RdrgContainer/>} >
             <Route path={HOME_ABSOLUTE_PATH} element = {<Home />} />
             <Route path={AUTH_ABSOLUTE_PATH} element = {<Authentication/>} />
             <Route path={SIGN_UP_ABSOLUTE_PATH} element = {<SignUp onLinkClickHandler={function (): void {
-                throw new Error('Function not implemented.');
-            } }/>} />
-            <Route path={CUSTOMER_SUPPORT_ABSOLUTE_PATH} element={<CustomerSupportList/>} />
+                throw new Error('Function not implemented.');} }/>} />
+            <Route path={CUSTOMER_SUPPORT_PATH} >
+                <Route index element={< CustomerSupportList/>} />
+                <Route path={CUSTOMER_SUPPORT_WRITE_ABSOLUTE_PATH} element={<SupportWrite/>}/>
+                <Route path={CUSTOMER_SUPPORT_DETAIL_PATH} element={<SupportDetail/>} />
+                <Route path={CUSTOMER_SUPPORT_UPDATE_PATH} element={<SupportUpdate/>} />
+            </Route>
             <Route path={MYPAGE_PROFILE_ABSOLUTE_PATH} element={<MypageProfile />}/>
         </Route>
     </Routes>
-  )
+    );
 }
 
 export default App;
---
+++ src/apis/board/dto/request/index.ts
@@ -0,0 +1,16 @@
+// description: Q&A 작성 Request Body DTO //
+export interface PostBoardRequestDto {
+    title: string;
+    contents: string;
+}
+
+// description: Q&A Comment Request Body DTO //
+export interface PostCommentRequestDto {
+    comment: string;
+}
+
+// description: Q&A 수정 Request Body DTO //
+export interface PutBoardRequestDto {
+    title: string;
+    contents: string;
+}
