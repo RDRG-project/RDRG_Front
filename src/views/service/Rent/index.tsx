aaa82075f27fcfcb00705181da31dc9d588e279b
JiSangChoe <chlwltkd123@gmail.com>
2024-05-20 13:38:49 +0900

    지상 오전 대여페이지

--- src/views/service/Rent/index.tsx
+++ src/views/service/Rent/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import './style.css'
 import RentSiteSelectBox from 'src/components/Selectbox/RentSiteSelectBox'
 import { useCookies } from 'react-cookie';
+import ReturnSiteSelectBox from 'src/components/Selectbox/ReturnSiteSelectBox';
+import RentSelectBox from 'src/components/Selectbox/RentItemSelectBox';
 
 //                    component                    //
 export default function Rent() {
@@ -9,26 +11,43 @@ export default function Rent() {
     //                    state                    //
     const [cookies] = useCookies();
     const [rentSelect, setRentSelect] = useState<string>('');
+    const [returnSelect, setReturnSelect] = useState<string>('');
+    // const [dateSelect, setDateSelect] = useState<string>('');
+    const [rentItem, setRentItem] = useState<string>('');
 
 
     //                    event handler                    //
     const onRentChangeHandler = (rentSelect: string) => {
         setRentSelect(rentSelect);
     };
+    const onReturnChangeHandler = (returnSelect: string) => {
+        setReturnSelect(returnSelect);
+    };
 
+    // const onDateChangeHandler = (dateSelect: string) => {
+    //     setDateSelect(dateSelect);
+    // };
+
+    const onRentItemChangeHandler = (rentItem: string) => {
+        setRentItem(rentItem);
+    }
     
 
     //                    render                    //
     return (
         <div id='rent-wrapper'>
             <div className='rent-left-side'>
-                <div className='rent-left-side-rental'>대여지점</div>
+                <div className='rent-left-side-rental'>
                     <RentSiteSelectBox value={rentSelect} onChange={onRentChangeHandler} />
-                <div className='rent-left-side-return'>반납지점</div>
-                    <RentSiteSelectBox value={rentSelect} onChange={onRentChangeHandler} />
+                </div>
+                <div className='rent-left-side-return'>
+                    <ReturnSiteSelectBox value={returnSelect} onChange={onReturnChangeHandler} />
+                </div>
                 <div className='rent-left-side-date'>날짜 및 시간</div>
             </div>
-            <div className='rent-item'>기기 선택</div>
+            <div className='rent-item'>
+                <RentSelectBox value={rentItem} onChange={onRentItemChangeHandler} />
+            </div>
             <div className='rent-right-side'>
                 <div className='rent-right-side-basket'>장바구니</div>
                 <div className='rent-right-sid-payment'>결제</div>
