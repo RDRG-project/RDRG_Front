5e8deba77d6819520efcac0f1659579366f48416
JiSangChoe <chlwltkd123@gmail.com>
2024-05-27 12:51:10 +0900

    대여 리스트 불러오기

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,5 +2,6 @@ import useUserStore from "./user.store";
 import useAuthenticationStore from "./authentication.store";
 import useBasketStore from "./basket.store"
 import useRentItemStore from "./rentItem.store"
+import useRentDateStore from './rent-date.store';
 
-export { useUserStore, useAuthenticationStore, useBasketStore, useRentItemStore };
+export { useUserStore, useAuthenticationStore, useBasketStore, useRentItemStore, useRentDateStore };
--- src/types/index.ts
+++ src/types/index.ts
@@ -30,12 +30,13 @@ export interface DeviceListItem {
 }
 
 export interface ItRentList {
-    serialNumber: string;
+    serialNumber?: string;
     model: string;
     name: string;
-    deviceExplain: string;
-    type: string;
+    // 설명 부분 좀 손봐야함
+    deviceExplain?: string;
+    type?: string;
     brand:string;
     price: number;
-    devicesImgUrl: string;
+    devicesImgUrl?: string;
 }
