76aa9075f178e8896c375b73dc1a964057fb63e3
JiSangChoe <chlwltkd123@gmail.com>
2024-05-28 17:33:52 +0900

    대여페이지 렌더링 구현중

--- src/components/Selectbox/RentSiteSelectBox/index.tsx
+++ src/components/Selectbox/RentSiteSelectBox/index.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
+import { useRentSiteStore, useReturnSiteStore } from 'src/stores';
 
+//                    interface                    //
 interface Prop {
     value: string;
     onChange: (value: string) => void;
@@ -8,43 +10,45 @@ interface Prop {
 
 //                    component                    //
 export default function RentSiteSelectBox({ value, onChange }: Prop) {
-
     const RentSiteListItem = [
-        { name : '서울', value: 'SEOUL'},
-        { name : '부산', value: 'BUSAN'}
+        { name: '서울', value: 'SEOUL' },
+        { name: '부산', value: 'BUSAN' }
     ];
 
     //                    state                    //
     const [show, setShow] = useState<boolean>(false);
-    const [name, setName] = useState<string>('');
+    const [selectedItem, setSelectedItem] = useState<string>('');
 
-    //                    event handler                    //
+    const { rentSite, setRentSite } = useRentSiteStore();
+
     const onButtonClickHandler = () => {
         setShow(!show);
     };
-    // 요소를 선택하면 색깔이 바뀌고 db에 대여장소를 저장하게 해야함 아래코드는 수정해야함
-    const onItemClickHandler = () => {
-        RentSiteListItem.forEach(item => {
-            if (item.value === value) setName(item.name);
-        })
-        onChange(value);
+
+    //                    event handler                    //
+    const onItemClickHandler = (itemValue: string) => {
+        setSelectedItem(itemValue);
+        setRentSite(rentSite);
+        onChange(itemValue);
         setShow(true);
     };
 
     //                    render                    //
-    const buttonClass = show ? 'select-close-button' : 'select-open-button';
     return (
         <div className='select-box'>
             <div className='select-item-title'>대여지점</div>
-            <div className={buttonClass} onClick={onButtonClickHandler}></div>
-            {show && 
-            <div className='select-list'>
-                {RentSiteListItem.map((item) => 
-                <div className='select-list-item-box' onClick={onItemClickHandler}>
-                    <div className='select-item'>{item.name}</div>
+            <div className={show ? 'select-close-button' : 'select-open-button'} onClick={onButtonClickHandler}></div>
+            {show &&
+                <div className='select-list'>
+                    {RentSiteListItem.map((item) =>
+                        <div
+                            key={item.value}
+                            className={selectedItem === item.value ? 'select-list-item-selected' : 'select-list-item-box'}
+                            onClick={() => onItemClickHandler(item.value)}>
+                            <div className='select-item'>{item.name}</div>
+                        </div>
+                    )}
                 </div>
-                )}
-            </div>
             }
         </div>
     );
