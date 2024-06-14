a5b06f2fa2b0683ffbf7e2f3c24a1a8474c186e8
Chixol <phw9278@gmail.com>
2024-06-14 17:20:20 +0900

    대여 페이지 css 수정 및 달력 수정

--- src/components/DateTimebox/index.tsx
+++ src/components/DateTimebox/index.tsx
@@ -34,7 +34,8 @@ const datePickerProps = (
     showPopperArrow: false,
     minDate: new Date(),
     dateFormat: "yyyy년 MM월 dd일",
-    customInput: <DateCustomInput value={startDate ? `${startDate.toLocaleDateString()} - ${endDate ? endDate.toLocaleDateString() : ''}` : placeholderText} />
+    customInput: <DateCustomInput value={startDate ? `${startDate.toLocaleDateString()} - ${endDate ? endDate.toLocaleDateString() : ''}` : placeholderText} />,
+    monthsShown: 2 // 두 달력을 동시에 표시
 });
 
 const ReactDatePicker: React.FC = () => {
